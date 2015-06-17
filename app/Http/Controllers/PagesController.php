8f0fe8b1d616b74593babae31edd48a0247fdbfa
Jayson Rael Ramos <lothlorien654@gmail.com>
2015-06-17 12:28:06 +0800

    Modify Pages Controller, add save function

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use App\Http\Requests;
 use Illuminate\Routing\Controller;
 use App\Tblusers;
 class PagesController extends Controller
 {
+    protected $req;
+    protected $table_users;
     public function index(){
         $request=new Request;
         return view('welcome');
     }
-    public function save(){
-
+    public function SignUp(){
+        $req=new Request;
+        $username=$req->input('uname');
+        $password=$req->input('pword');
+        $table_users=new Tblusers;
+        $table_users->username=$username;
+        $table_users->password=$password;
+        $table_users->created_at=new \DateTime();
+        $table_users->updated_at=new \DateTime();
+        $table_users->save();
     }
     public function home(){
         return view('home');
