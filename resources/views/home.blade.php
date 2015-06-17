220cfeafb8593768daa18b92516292096b2938d2
Jayson Rael Ramos <lothlorien654@gmail.com>
2015-06-17 17:12:10 +0800

    Integrate Elixir

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,18 @@
 <!DOCTYPE html>
         <html>
             <head>
-                {!! HTML::style('css/bootstrap-theme.css') !!}
-                {!! HTML::style('css/bootstrap.css.css') !!}
+                {!! HTML::style('bootstrap/css/bootstrap-theme.css') !!}
+                {!! HTML::style('bootstrap/css/bootstrap.css.css') !!}
             </head>
             <body>
+            <div>
 
+            </div>
+            <article class="container-fluid">
 
-                <ul class="sidebar-nav">
-                    <li class="sidebar-brand">
-                        <a href="#">
-                            MICP Laravel Sample
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#">Overview</a>
-                    </li>
-                    <li>
-                        <a href="#">Events</a>
-                    </li>
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
+            </article>
+            <footer>
 
-
+            </footer>
             </body>
         </html>
