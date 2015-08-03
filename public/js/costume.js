cb0105721498665649e7684aa35aec2e200a5f1a
Jayson Rael Ramos <lothlorien654@gmail.com>
2015-08-03 15:07:11 +0800

    Create Costume.js

--- public/js/costume.js
+++ public/js/costume.js
@@ -1,15 +1,63 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: Jayson Ramos
- * Date: 7/30/15
- * Time: 1:35 PM
- * To change this template use File | Settings | File Templates.
- */
 $.noConflict();
+
 !function($){
     $(function(){
-         $("li").click(function(){
-             $(this).toggleClass('active','item');
-         });
+        $.ajaxSetup({
+            headers: { 'X-CSRF-Token' : $('meta[name=csrf-token]').attr('content') }
+        });
+        $(".datetimepicker").datetimepicker({
+            mask:true,
+            format:'Y/m/d H:i',
+            timepicker:true,
+            datepicker:true
+        });
+        $(".btn-success").click(function(){
+            var news_title=$("#title").val();
+            var content=$("#content").val();
+            var created_at=$("#created_at").val();
+            var _token=$('input[name=_token]').val();
+            //create our slug
+            var slug=news_title.toLowerCase().replace(/([\^\!@\+#\$%^\,\.\'\"&*\s]){1,}/g,"-");
+            //our data string
+            var dataString="title="+news_title+"&slug="+slug+"&content="+content+"&created_at="+created_at+"&_token="+_token;
+            //ajax here
+            $.ajax({
+                url:"/news/store",
+                type: "POST",
+                data:dataString
+                ,success:function(data){
+                    //if the data was validated without errors
+
+                    if(typeof data=="string"){
+                        if($(".alert").hasClass("alert-danger")){
+                            $(".alert").removeClass("alert-danger");
+                        }
+                        $(".alert").addClass("alert-success").text(data).fadeIn(2000).fadeOut(7000);
+                    }
+                    //if there are errors in validation
+                    else if(typeof data=="object"){
+                        if($(".alert").hasClass("alert-success")){
+                            $(".alert").removeClass("alert-success");
+                        }
+                        $(".alert").addClass("alert-danger");
+                        for(var i=data.length-1; i>0; i--){
+                            $(".alert").text(data[i]+$(".alert").text()).fadeIn(2000).fadeOut(7000);
+                        }
+                    }
+                    else{
+                        console.log(data);
+                    }
+                },
+                error:function(xhr,status){
+                    if($(".alert").hasClass("alert-success")){
+                        $(".alert").removeClass("alert-success");
+                    }
+                    $(".alert").addClass("alert-danger");
+                    $(".alert").text("An error occurred when the data was submitted." + xhr.responseJSON).fadeIn(2000).fadeOut(7000);
+                }
+
+            });
+
+        });
     });
-}(jQuery);
+}(window.jQuery);
